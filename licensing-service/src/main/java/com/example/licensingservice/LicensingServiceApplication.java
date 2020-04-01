51d11b2db1dfb692544e3f54c14c5e90c5171e4d
Daniel Rojas <dfelipe2@gmail.com>
2020-04-01 00:29:38 -0500

    added confsvr

--- licensing-service/src/main/java/com/example/licensingservice/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/example/licensingservice/LicensingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LicensingserviceApplication {
+public class LicensingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LicensingserviceApplication.class, args);
+		SpringApplication.run(LicensingServiceApplication.class, args);
 	}
 
 }
